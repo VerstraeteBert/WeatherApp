3e20ea954df5dca9aacb5dcd1e3507e132b45b5f
VerstraeteBert <bert-verstraete@hotmail.com>
2018-11-04 16:47:34 +0100

    Added migration

---
+++ migrations/1_initial_migration_up.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS `weatherdb`;
+
+CREATE TABLE IF NOT EXISTS `readings` (
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`timestamp` datetime NOT NULL,
+	`degreescelcius` decimal(10,2) NOT NULL,
+	PRIMARY KEY(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
